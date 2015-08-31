60e9b5ca43a8debd83734222b3d591c9368ebdbc
simplicitesoftware <contact@simplicitesoftware.com>
2015-08-31 11:40:18 +0200

    Updated

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
 
-URL="http[s]://<base url of your Simplicite(R) instance>/health"
+#=========================================================
+# Monitored base URL
+URL="http[s]://<base url of your Simplicite(R) instance>"
+# Mail account (i.e. mailx account in $HOME/.mailrc), no emails if left empty
+MAIL=operation
+# Destination email
+DEST=support@yourdomain.com
+#=========================================================
 
 DATE=`date +%Y-%m-%d:%H:%M:%S`
-
 BASENAME=`basename $0 .sh`
-
 LOG=$BASENAME.log
 [ ! -f $LOG ] && touch $LOG
-
 CSV=$BASENAME.csv
 [ ! -f $CSV ] && touch $CSV
-
 RES=$BASENAME.res
-
 COOKIES=$BASENAME.cookies
 
-curl -k -s -b $COOKIES -c $COOKIES -i $URL > $RES
+curl -k -s -b $COOKIES -c $COOKIES -i "$URL/health" > $RES
 CODE=$?
 if [ $CODE != 0 ]
 then
-        echo "Unable to call URL $URL, curl exit code = $CODE" >&2
-        exit -1
+        MSG="Unable to call URL $URL, curl exit code = $CODE"
+	[ "$MAIL" != "" ] && echo "$DATE - $MSG" | mailx -A $MAIL -s "[Simplicite(R) operation] Error" $DEST
+        echo $MSG >&2
+        exit 1
 fi
 
 STATUS=`head -1 $RES | awk '{print $2}'`
 
-CODE=0
-DATA=""
+MSG="HTTP status $STATUS for URL $URL"
 if [ $STATUS == "200" ]
 then
         HEAPSIZE=`grep '^HeapSize' $RES | awk -F= '{print $2}'`
         HEAPFREE=`grep '^HeapFree' $RES | awk -F= '{print $2}'`
         HEAPMAX=`grep '^HeapMaxSize' $RES | awk -F= '{print $2}'`
-        DATA=" - `grep '^Status=' $RES`, HeapSize=$HEAPSIZE, HeapFree=$HEAPFREE, HeapMaxSize=$HEAPMAX"
         echo "$HEAPSIZE;$HEAPFREE;$HEAPMAX" >> $CSV
+	echo "$DATE - $MSG - `grep '^Status=' $RES`, HeapSize=$HEAPSIZE, HeapFree=$HEAPFREE, HeapMaxSize=$HEAPMAX" | tee $LOG
+	exit 0
 else
-        CODE=1
+	[ "$MAIL" != "" ] && echo "$DATE - $MSG" | mailx -A $MAIL -s "[Simplicite(R) operation] Error" $DEST
+        echo $MSG >&2
+        exit 2
 fi
-
-echo "$DATE - HTTP status $STATUS$DATA" | tee $LOG
-exit $CODE
